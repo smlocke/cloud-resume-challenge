c4edb82182c5b5c7b44258c117706d6d2cbfbbea
smlocke <72567531+smlocke@users.noreply.github.com>
2022-09-04 09:13:24 -0600

    Create increment-o-nator.py
    Initial add of lambda python counting function

---
+++ back-end/count-function/increment-o-nator.py
@@ -0,0 +1,50 @@
+import boto3
+import json
+import uuid
+
+# DynamoDB table name
+table_name = "count-01"
+
+# Get the service resource
+dynamodb = boto3.resource('dynamodb')
+
+# Create service client
+db_client = boto3.client('dynamodb')
+
+# Resource representing an Amazon DynamoDB Table
+table = dynamodb.Table(table_name)
+
+
+# Function to update the count
+def updateCount():
+    response = db_client.update_item(
+        TableName = table_name,
+        Key = {
+            'PK': {'N': "0"}
+        },      
+        UpdateExpression= "ADD visitor :inc",
+        ExpressionAttributeValues={":inc": {"N": "1"}}
+    )
+
+# Function to retrieve the count
+def getCount():
+    item = table.get_item(
+        Key = {
+            "PK": 0
+        }
+    )
+    visitcount = (item["Item"])["visitor"]
+    return visitcount
+
+
+def lambda_handler(event, context):
+    updateCount()
+    return getCount()
+    return {
+        "statusCode": 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*'
+        }
+    }    
